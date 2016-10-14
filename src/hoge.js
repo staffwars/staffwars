63e6e44c531da482340158880acbbea7fbb13926
Naoki Iwata <iwata-na@gnavi.co.jp>
2016-10-14 10:04:09 +0900

    milkcocoaを追加

--- src/hoge.js
+++
@@ -1,5 +0,0 @@
-export default class Hoge {
-    fuga() {
-        console.log('fuga');
-    }
-}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import hoge from "./hoge.js"
-console.log('test code');
+// MilkCocoa
+const milkcocoa = new MilkCocoa('guitariu6e7lgx.mlkcca.com');
+const ds = milkcocoa.dataStore('messages');
+
+ds.on('send', function(data) {
+  console.log(data);
+});
+
+console.log('start')
