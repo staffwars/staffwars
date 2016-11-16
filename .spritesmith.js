991e4076176e3a0b07477af2acc9a51e93adc7c2
rnaxu <luv.s2.rnaxu@gmail.com>
2016-11-17 02:23:57 +0900

    spriteをpackage.jsonのscriptに追加

---
+++ .spritesmith.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var util = require('util');
+
+module.exports = {
+  src: 'src/sprite/*.{png,gif,jpg}',
+  destImage: 'src/img/sprite.png',
+  destCSS: 'src/scss/setting/_sprite.scss',
+  imgPath: '../img/sprite.png',
+  padding: 4,
+  cssFormat: 'scss'
+};
